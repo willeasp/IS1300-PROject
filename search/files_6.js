59a8f6ae6e5761092b602449747f60908dc6bb3a
willeasp <willeasp@users.noreply.github.com>
2022-01-03 16:18:31 +0000

    deploy: ebb0ef5a57b9bbf07f3cef7c2900aef011e25204

--- search/files_6.js
+++ search/files_6.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['usart_2ec_0',['usart.c',['../usart_8c.html',1,'']]]
+  ['uart_2ec_0',['uart.c',['../uart_8c.html',1,'']]],
+  ['usart_2ec_1',['usart.c',['../usart_8c.html',1,'']]]
 ];
